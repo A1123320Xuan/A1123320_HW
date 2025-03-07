e3238ffc19288d7971973d435005e6a52d374ab6
XuanBoXu <a1123320@mail.nuk.edu.tw>
2025-03-07 16:04:50 +0800

    Add files via upload

---
+++ a1123320_test.php
@@ -0,0 +1,27 @@
+<html>
+
+<head></head>
+
+<body>
+
+<form action = "test2.php" method = "GET" target = "new" >
+    Please input your name : <input type = "text" name = "uNAME" required><br>
+    Please input your password : <input type = "password" name = "uPW" required><br>
+    Please input your email : <input type = "email" name = "uML" required><br>
+    Please select your color : <input type = "color" name = "uCL" required><br>
+    Please select your birthday : <input type = "date" name = "uDT" required><br>
+    Please input your webpage : <input type = "url" name = "uWP" required><br>
+    Please select how do you like the webpage : <input type = "range" name = "uRG" required><br>
+    Please select yor gender : <input type = "radio" name = "uGD" value = "male" required>male
+    <input type = "radio" name = "uGD" value = "female" required>female<br>
+    <input type = "hidden" name = "uSC" value = "LOL" required >
+    Please select yor gender :
+    <input type = "checkbox" name = "uIT[]" value = "sport" required>sport
+    <input type = "checkbox" name = "uIT[]" value = "music" required>music
+    <input type = "checkbox" name = "uIT[]" value = "painting" required>painting<br>
+    <input type = "submit"><input type = "reset">
+</form>
+
+</body>
+
+</html>
