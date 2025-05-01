bcd6177cc8da19bcdb101ce0e839d8c6b07d818b
XuanBoXu <a1123320@mail.nuk.edu.tw>
2025-05-02 04:56:24 +0800

    Update sendMail.php

--- sendMail.php
+++ sendMail.php
@@ -1,12 +1,4 @@
 <?php
-$to = $_POST["to"];
-$subject = $_POST["subject"];
-$content = nl2br($_POST["content"]);
-
-echo $to;
-echo $subject;
-echo $content;
-
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
@@ -15,41 +7,54 @@ require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 
+$to = $_POST["to"];
+$subject = $_POST["subject"];
+$content = nl2br($_POST["content"]);
+$content_raw = $_POST["content"];
+
 $mail = new PHPMailer(true);
 
 try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'a1123320@mail.nuk.edu.tw';                     //SMTP username
-    $mail->Password   = 'lpbf bztd pzzp woki';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.gmail.com';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'a1123320@mail.nuk.edu.tw';
+    $mail->Password   = 'lpbf bztd pzzp woki';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    $mail->Port       = 465;
 
-    //Recipients
     $mail->setFrom('a1123320@mail.nuk.edu.tw', 'Mailer');
-    //$mail->addAddress('aa159951753@gmail.com', 'Joe User');     //Add a recipient
     $mail->addAddress('aa159951753@gmail.com'); 
-    $mail->addAddress($to);             //Name is optional
-    //$mail->addReplyTo('info@example.com', 'Information');
-    //$mail->addCC('cc@example.com');
-    //$mail->addBCC('bcc@example.com');
+    $mail->addAddress($to); 
 
-    //Attachments
-    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->isHTML(true);
     $mail->Subject = $subject;
-    $mail->Body    = $content."</b>";
-    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    $mail->Body    = $content;
 
     $mail->send();
-    echo 'Message has been sent';
+    echo 'Email 已成功寄出<br>';
 } catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    echo "寄信失敗: {$mail->ErrorInfo}<br>";
 }
+
+// 儲存到資料庫
+$link = new mysqli('localhost', 'root', '', 'webpage');
+mysqli_set_charset($link, 'utf8');
+
+if ($link->connect_error) {
+    die("資料庫連線失敗：" . $link->connect_error);
+}
+
+$stmt = $link->prepare("INSERT INTO mail (email, subject, content) VALUES (?, ?, ?)");
+$stmt->bind_param("sss", $to, $subject, $content);
+
+
+if ($stmt->execute()) {
+    echo "資料已儲存至資料庫";
+} else {
+    echo "資料儲存失敗：" . $stmt->error;
+}
+
+$stmt->close();
+$link->close();
 ?>
